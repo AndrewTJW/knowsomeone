eccf461625cc6400952f2170f96189276a6510e6
AndrewTJW <130200621+AndrewTJW@users.noreply.github.com>
2025-03-28 17:02:56 +0800

    Updated

--- backend/backend.js
+++ backend/backend.js
@@ -8,10 +8,10 @@ const port = 3000;
 
 //enables CORS [Cross Origin Resource Sharing]
 app.use(cors({
-    origin: "https://knowsomeone-gfy6q7tvn-andrews-projects-48fa8a40.vercel.app",
+    origin: "https://knowsomeone-eight.vercel.app/",
     credentials: true,
-    methods: "GET,POST,PUT,DELETE",  // Explicitly include PUT & DELETE
-    allowedHeaders: "Content-Type,Authorization", // Include authentication headers
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization"
 }));
 
 //act as a middleware
